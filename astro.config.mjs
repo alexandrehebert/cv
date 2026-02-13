a4307ecbb9a82243a1b12623fe4a6b45ed9a9bab
Alexandre Hebert <hebert.alex@gmail.com>
2026-02-12 20:12:36 -0500

    fix: base href

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  base: process.env.BASE_HREF || "/",
   output: "static",
   integrations: [tailwind()],
 });
